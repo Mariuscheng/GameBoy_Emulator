9193a893b27ee4748e7cad26abc46c2b4418ef6c
Mariuscheng <47233413+Mariuscheng@users.noreply.github.com>
2025-07-15 18:04:19 +0800

    Delete src directory

--- src/interface/mod.rs
+++
@@ -1,9 +0,0 @@
-// External interface module
-
-pub mod input;
-pub mod video;
-pub mod audio;
-
-pub use input::{Joypad, GameBoyKey};
-pub use video::VideoInterface;
-pub use audio::AudioInterface;
