ad741b585807d101360228187eb9d7481ebc2e40
Mariuscheng <47233413+Mariuscheng@users.noreply.github.com>
2025-08-12 18:35:54 +0800

    Delete src directory

--- src/interface/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod audio;
-pub mod input;
-pub mod sdl3_display;
-pub mod video;
-pub mod mock_display;
-
-// 若未來有 input/joypad.rs 可加 pub mod input;
--- src/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod audio;
-pub mod core;
-pub mod error;
-pub mod interface;
-pub mod mmu;
-pub mod sdl3_display;
-pub mod utils;
-// If you want to re-export rom_header, use:
-pub use crate::utils::rom_header::*;
