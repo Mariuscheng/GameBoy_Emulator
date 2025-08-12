56120202f4027f68e9adc7f328745f89b6574bd6
Mariuscheng <47233413+Mariuscheng@users.noreply.github.com>
2025-08-12 18:36:09 +0800

    Delete target directory

--- target/debug/deps/gameboy_emulator.d
+++
@@ -1,5 +0,0 @@
-C:\Users\mariu\Desktop\Rust\gameboy_emulator\target\debug\deps\gameboy_emulator.exe: src\main.rs
-
-C:\Users\mariu\Desktop\Rust\gameboy_emulator\target\debug\deps\gameboy_emulator.d: src\main.rs
-
-src\main.rs:
--- target/debug/gameboy_emulator.d
+++
@@ -1,1 +0,0 @@
-C:\Users\mariu\Desktop\Rust\gameboy_emulator\target\debug\gameboy_emulator.exe: C:\Users\mariu\Desktop\Rust\gameboy_emulator\src\main.rs
